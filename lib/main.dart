dc80a87d45160bf73b9848b2622c372702b0fce6
Rajesh Rajappan <rajesh.rajappan@trant.co.uk>
2020-11-29 21:02:47 +0000

    Add business card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,97 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.white,
+                backgroundImage: AssetImage('images/raj.jpeg'),
+              ),
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'Rajesh Rajappan',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(
+                  'SOFTWARE DEVELOPER',
+                  style: TextStyle(
+                    color: Colors.blueGrey.shade100,
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    letterSpacing: 3,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 60.0,
+                  vertical: 20.0
+                ),
+                color: Colors.grey,
+                height: 1.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone_android,
+                    color: Colors.blueGrey.shade700,
+                  ),
+                  title: Text(
+                    '+44 7545 330804',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade700,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.blueGrey.shade700,
+                  ),
+                  title: Text(
+                    'mail@example.com',
+                    style: TextStyle(
+                      color: Colors.blueGrey.shade700,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
